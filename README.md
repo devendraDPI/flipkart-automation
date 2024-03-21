be38f53c692443bf52f2c09c61ddfc4e99182624
Atri Biswas <atribiswas3009@gmail.com>
2024-03-21 13:27:17 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Project Name - _Project Author (Change Me)_
+
+## Project Name and Description:
+A concise yet descriptive title and an overview of the project’s purpose.
+
+## Installation Instructions:
+Clear steps to set up and run the project locally.
+> Example:
+```
+# java version 17
+java --version
+```
+
+## Usage and Examples:
+Provide sample code or usage scenarios to showcase how the project works.
+> Example:
+```
+# to run the project
+./gradlew run
+```
+
+## Important Links:
+Details about useful external links
+ 
---
+++ src/main/java/demo/TestCases.java
@@ -0,0 +1,34 @@
+package demo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestCases {
+    ChromeDriver driver;
+    public TestCases()
+    {
+        System.out.println("Constructor: TestCases");
+        WebDriverManager.chromedriver().timeout(30).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+    
+    public  void testCase01(){
+        System.out.println("Start Test case: testCase01");
+        driver.get("https://www.google.com");
+        System.out.println("end Test case: testCase02");
+    }
+
+
+}
